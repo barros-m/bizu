2cf987a8f3a15ae27c49bb104dd9ffd60ada888b
Matheus Barros <matheusbarros1305@gmail.com>
2023-05-21 01:03:36 -0400

    Hero new text and logo on it.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import video from '../../assets/videos/home_video_miami_beach.mp4';
+import logo from '../../assets/images/bizu_logo.png';
 import './Hero.css';
 
-
 const HeroSection = () => {
-    return (
-<section className="hero">
-  <video className="hero-video" autoPlay muted loop>
-    <source src={video} type="video/mp4" />
-  </video>
-  <div className="hero-overlay"></div>
-  <div className="hero-content">
-    <h1 className="hero-title">Empower Your Business with BIZU Agency</h1>
-    <p className="hero-description">Innovative Digital Marketing Solutions Tailored to Your Needs</p>
-  </div>
-</section>
+  return (
+    <section className="hero">
+      <video className="hero-video" autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="hero-overlay"></div>
+      <div className="hero-content">
+        <img src={logo} alt="Logo" className="hero-logo" />
+        <h1 className="hero-title">New era of Marketing.</h1>
+        <p className="hero-description">Quality over quantity.</p>
+      </div>
+    </section>
+  );
+};
 
-    );
-  };
-  
-  export default HeroSection;
+export default HeroSection;
