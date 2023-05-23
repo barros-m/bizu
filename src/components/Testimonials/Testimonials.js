ad03f640e37a21188c4d398280b925fc21ff506f
Matheus Barros <matheusbarros1305@gmail.com>
2023-05-23 19:40:08 -0400

    Bruna por Chris

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -18,14 +18,14 @@ const Testimonials = () => {
       text: "I had a great experience working with BIZU for my gym. Their team was knowledgeable and professional, and they developed a customized strategy that helped me connect with potential clients and increase revenue. Their social media campaigns were particularly effective in targeting the right audience and growing my following. They also provided valuable guidance throughout the process and were always available to answer my questions. Overall, I was impressed with the agency's expertise and dedication to their clients. They helped me achieve my business goals and I would recommend them to any small business looking to improve their digital presence.",
       author: "Bruna B.",
       position: "XN CEO & Founder", 
-      image: img_chris
+      image: img_bruna
     },
     {
       id: 3,
       text: "BIZU agency helped me improve my Instagram and attract more clients to my tattoo artistry business. Their customized strategy and engaging content increased my following and client base. Their valuable guidance and expertise are unmatched, and I highly recommend them to any small business looking to improve their social media presence.",
       author: "Chris G",
       position: "Tattoo Artist", 
-      image: img_bruna
+      image: img_chris
     }
     // Additional testimonials can be added here
   ];
