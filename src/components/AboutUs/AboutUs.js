bf85e0610a4b9359cf5ce2c39f9bff5b04f146ce
Matheus Barros <matheusbarros1305@gmail.com>
2023-05-26 08:56:52 -0400

    Fixed about us

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -9,7 +9,7 @@ const AboutUs = () => {
       <div className="about-us-content">
       <div className='section-title'>About Us</div>
         <p className="about-us-description">
-        BIZU Agency is dedication to the highest quality of digital marketing - is a unique combination of creativity, strategy, flexibility, and productivity. We have the solution by applying our digital marketing expertise and personalized strategy for your business. We believe good enough just isn’t enough. Our goal is to ensure our clients are easily satisfied with the very best.
+        BIZU Agency is dedicated to the highest quality of digital marketing - is a unique combination of creativity, strategy, flexibility, and productivity. We have the solution by applying our digital marketing expertise and personalized strategy for your business. We believe good enough just isn’t enough. Our goal is to ensure our clients are easily satisfied with the very best.
 
         <br/><br/> Our purpose is to create memorable experiences. By changing the industry and setting new norms of professionalism and success, we will be global leaders in digital marketing services. Our main goal is to become one of the top marketing companies in the world, providing marketing strategies, digital sales solutions, and brand management. Miami Based. Internationally Known.
 
