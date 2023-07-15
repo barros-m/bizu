9625455edfd6cba089f03b8f7c66fe62714fec99
Matheus Barros <matheusbarros1305@gmail.com>
2023-07-15 15:19:00 -0400

    Added new client

--- src/components/Portfolio/NewPortfolio/PortfolioGrid.js
+++ src/components/Portfolio/NewPortfolio/PortfolioGrid.js
@@ -22,46 +22,37 @@ import kiko0 from '../../../assets/images/porfolio/kiko_tattoo_rio_0.png'
 import kiko1 from '../../../assets/images/porfolio/kiko_tattoo_rio_1.png'
 import kiko2 from '../../../assets/images/porfolio/kiko_tattoo_rio_2.png'
 
+import tmr0 from '../../../assets/images/porfolio/tmr_barbershop_0.jpeg'
+import tmr1 from '../../../assets/images/porfolio/tmr_barbershop_1.jpeg'
+import tmr2 from '../../../assets/images/porfolio/tmr_barbershop_2.jpeg'
+
 function PortfolioGrid() {
 
     const portfolio = [
-        // { src: ju0, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
-        // { src: bizu0, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
-        // { src: xande0, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
-        // { src: gringa0, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
-        // { src: kiko0, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
 
-        // { src: ju1, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
-        // { src: bizu1, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
-        // { src: xande1, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
-        // { src: gringa1, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
-        // { src: kiko1, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
+        { src: bizu1, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
+        { src: bizu0, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
+        { src: bizu2, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
+        
+        { src: gringa0, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
+        { src: gringa1, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
+        { src: gringa2, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
+        
+        { src: ju1, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
+        { src: ju0, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
+        { src: ju2, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
 
-        // { src: ju2, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
-        // { src: bizu2, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
-        // { src: xande2, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
-        // { src: gringa2, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
-        // { src: kiko2, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
+        { src: kiko1, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
+        { src: kiko0, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
+        { src: kiko2, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
 
-  { src: bizu1, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
-  { src: bizu0, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
-  { src: bizu2, alt: 'BizuAgency', details: 'Bizu Agency', url: '' },
-  
-  { src: gringa0, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
-  { src: gringa1, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
-  { src: gringa2, alt: 'GringaBrasileira', details: 'Gringa Brasileira', url: 'https://www.instagram.com/gringa.brasileiraa/' },
-  
-  { src: ju1, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
-  { src: ju0, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
-  { src: ju2, alt: 'JulianaReyPsychologist', details: 'Juliana Rey Psychologist', url: 'https://instagram.com/psijulianarey' },
+        { src: xande0, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
+        { src: xande2, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
+        { src: xande1, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
 
-  { src: kiko1, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
-  { src: kiko0, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
-  { src: kiko2, alt: 'KikoTattoo', details: 'Kiko Tattoo', url: 'https://www.instagram.com/kikotattoorio/' },
-
-  { src: xande0, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
-  { src: xande2, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
-  { src: xande1, alt: 'Xande', details: 'XN Studio', url: 'https://instagram.com/xandenegao/' },
+        { src: tmr0, alt: 'TMRBarbershop', details: 'The Men\'s Room Barbershop', url: 'https://instagram.com/tmrbarbershop.miami' },
+        { src: tmr1, alt: 'TMRBarbershop', details: 'The Men\'s Room Barbershop', url: 'https://instagram.com/tmrbarbershop.miami' },
+        { src: tmr2, alt: 'TMRBarbershop', details: 'The Men\'s Room Barbershop', url: 'https://instagram.com/tmrbarbershop.miami' },
 
         // add more images as needed
     ];
